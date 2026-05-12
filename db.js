1896948dd28d430719f337688165928dbf38e9a9
P. Gayathri Priya Darshini <gayathri7524@gmail.com>
2026-05-13 02:08:16 +0530

    Add files via upload

---
+++ db.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+pool.connect()
+  .then(() => {
+    console.log("Database Connected");
+  })
+  .catch((err) => {
+    console.log("Database Connection Error", err);
+  });
+
+module.exports = pool;
